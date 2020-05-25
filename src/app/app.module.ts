83abea54d5484822cb2d63240dcd1a9f4ac3b04c
bradohorsley <bradohorsley@aim.com>
2020-05-25 17:20:11 +0100

    6 files modified, 4 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ 
+   BrowserModule,
+   BrowserAnimationsModule,
+   FormsModule,
+   HttpClientModule,
+   MatToolbarModule,
+   MatIconModule,
+   MatButtonModule,
+   MatSidenavModule,
+   MatListModule,
+  ],
+  declarations: [ AppComponent, PagesComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/models/my-pages.ts
@@ -0,0 +1,6 @@
+export interface MyPages {
+  id: number;
+  title: string;
+  content: string;
+  slug: string;
+}
